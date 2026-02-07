11911da13ba51cb548df72eef6911db5bf593c11
Chris Vilches <felovilches@gmail.com>
2026-02-07 23:41:14 +0900

    cli modularization

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,56 @@
 
 inline constexpr const DIR_TYPE default_dir_type = DIR_TYPE::LOCAL_STATE;
 
+class CLIUsageError : public std::exception {
+ public:
+  explicit CLIUsageError(const std::string& msg) : message(std::move(msg)) {}
+
+  const char* what() const noexcept override { return message.c_str(); }
+
+ private:
+  std::string message;
+};
+
+class CLIArgs {
+ public:
+  CLIArgs(const int argc, const char* const argv[]) {
+    if (argc != 2) {
+      throw CLIUsageError(std::format(
+          "needs a unique key to identify the content (got {} arguments)", argc - 1));
+    }
+
+    key = parse_key(argv[1]);
+    if (!key.has_value()) {
+      throw CLIUsageError(std::format(
+          "key should be non-empty alphanumeric (can contain dash and underscore)"));
+    }
+  }
+
+  const std::string& get_key() const { return key.value(); }
+
+ private:
+  std::optional<std::string> key;
+};
+
 int main(const int argc, const char* const argv[]) {
-  if (argc != 2) {
-    std::cerr << std::format(
-        "needs a unique key to identify the content (got {} arguments)\n\n", argc - 1);
+  try {
+    CLIArgs args(argc, argv);
+
+    const std::string file_path = get_full_file_path(default_dir_type, args.get_key());
+    const std::optional<std::string> prev_content = read_file(file_path);
+    const std::string new_content = read_stdin();
+
+    const bool changed = get_changed(prev_content, new_content);
+
+    write_file(file_path, new_content);
+    return get_exit_code(changed);
+  } catch (const std::runtime_error& e) {
+    std::cerr << "Unexpected error:" << std::endl;
+    std::cerr << e.what() << std::endl;
+    return 2;
+  } catch (const CLIUsageError& e) {
+    std::cerr << e.what() << "\n\n";
     print_usage();
     return 2;
   }
-
-  const std::optional<std::string> key = parse_key(argv[1]);
-  if (!key.has_value()) {
-    std::cerr << std::format(
-        "key should be non-empty alphanumeric (can contain dash and underscore)\n\n");
-    print_usage();
-    return 2;
-  }
-
-  const std::string file_path = get_full_file_path(default_dir_type, key.value());
-  const std::optional<std::string> prev_content = read_file(file_path);
-  const std::string new_content = read_stdin();
-
-  const bool changed = get_changed(prev_content, new_content);
-
-  write_file(file_path, new_content);
-
-  return get_exit_code(changed);
 }
