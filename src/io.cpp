7c83ced143f9820cf2d3db2f733e04b0e31d10cc
Chris Vilches <felovilches@gmail.com>
2026-02-10 22:41:14 +0900

    refactor

--- src/io.cpp
+++ src/io.cpp
@@ -7,13 +7,17 @@
 // write permission? execution?). It's necessary to tell the user in what situation the
 // error happened.
 [[noreturn]] void handle_filesystem_errno(const std::string& path) {
+  std::string msg;
+
   if (errno == ENOENT) {
-    throw std::runtime_error(format("File or directory does not exist ({})", path));
+    msg = format("File or directory does not exist ({})", path);
   } else if (errno == EACCES) {
-    throw std::runtime_error(format("Permission denied ({})", path));
+    msg = format("Permission denied ({})", path);
   } else {
-    throw std::runtime_error(format("Unexpected open error ({})", path));
+    msg = format("Unexpected open error ({})", path);
   }
+
+  throw std::runtime_error(msg);
 }
 
 std::optional<std::string> read_file(const std::string& path) {
